0e11eb4e0f8bf503ddde0a7a3b1930e8c9e28551
Carla Holcomb <carla.programmer@gmail.com>
2020-06-14 02:39:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers = function(activity="go to the office") {
-  return `This Monday, I will ${activity}.`
+const returnFirstTwoDrivers = function(array) {
+  return array.first
 }
